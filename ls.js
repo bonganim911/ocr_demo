66d615d7e5e289fcff39b0acb8537e399d0765e7
Rohit Choudhary <rohit_choudhary@mckinsey.com>
2019-03-20 21:24:45 +0530

    feat: add detect-text route

---
+++ ls.js
@@ -0,0 +1,71 @@
+CUJUMU :  0
+OLIUI :  1
+Company Name LAMA TRADING COMPANY LLC :  2
+Tiade License No. XLV1201 :  3
+according to Trade License :  4
+Annual Turnover 1OOMILLION No. of Employees 105 Company Website LAWA.COM :  5
+Legal Status of Business :  6
+Residency X Resident :  7
+Non-Resident of non-resident country of domicile) :  8
+Specific Nature of Business I T Services :  9
+Business Address for at official communication) :  10
+Building Name BUILDING X :  11
+Shop/Apartment Office No 125 PO Box 0101 :  12
+Locality/Area DUBAI :  13
+Street Name/No DUBAI STREET :  14
+Landmark LANDMARK X :  15
+City DIFC :  16
+Emirate :  17
+Listed Telephone No OS 6 1 2 3 4 5 6 7 :  18
+Fax 0968976543 :  19
+Mobile No. (For sending alerts :  20
+E-mail ID (Mandatoryl lama@lama.com :  21
+This address will also be used to send :  22
+and wife :  23
+Registered Address i s provided tout from bude :  24
+both :  25
+Building Name lama stationary :  26
+Shop/Apartment Office No 137 PO Box O101 :  27
+Locality/Area DURIG :  28
+Street Nomoror No Mond Street, :  29
+Landmark Nohel Avenue City DUBIE :  30
+Country UAE :  31
+Listed Telephone No 0561 234 5 6 7 :  32
+Emil (Mandatory James-Peterson elama.com :  33
+VAT Value Added Tax) Registration details :  34
+Tax Registration Number (IRNI TRNOOI :  35
+taxistration Number (TRN) effective date 20 Jary 2 :  36
+Tax Registration AddressFantastic Building, finacial City :  37
+Tax Stato Dubai :  38
+Dubai :  39
+Details of Proprietors/Partners/Directors PA HO :  40
+Name James :  41
+Mobile Number 056 123 4567 :  42
+Phone Number osb 456 7123 :  43
+E-mail Address Jomes@loma.com :  44
+Mailing Address james box 123 :  45
+Place of Birth Germany :  46
+
+Name Albert Smith :  47
+Mobile Number 056 987 6543 :  48
+Phone Number 056 6543 987 :  49
+E-mail Address asmith@lama.com :  50
+Mailing Address :  51
+Place of Birth :  52
+
+Name Mond Ali :  53
+Mobile Number 056 127 9871 :  54
+Phone Number o 56 9071 271 :  55
+E-mail Address mohd Al@lama.com :  56
+Mailing Address :  57
+Place of Birth Dubai :  58
+
+Name Tey Wilson :  59
+Mobile Number 075 127 1123 :  60
+Phone Number 081 334 4446 :  61
+E-mail Address taywilson@lama.com :  62
+Mailing Address :  63
+Place of Birth South Africa :  64
+
+
+ :  65
