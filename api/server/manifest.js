636a20a660c4a58a17f567ab049958fdbf056470
bonganim911 <bonganim911@gmail.com>
2019-04-22 15:56:39 +0400

    Changed the cors.

--- api/server/manifest.js
+++ api/server/manifest.js
@@ -18,7 +18,7 @@ module.exports = new Confidence.Store({
         },
         routes: {
         cors: {
-          origin: ['http://3.18.113.163:3000'],
+          origin: ['*'],
         }
       },
         debug: {
